95ad279192ade364824410d5a5f6bdb3c7ddfd09
wangyuzhi233 <78699242+wangyuzhi233@users.noreply.github.com>
2021-04-06 04:37:25 +0800

    add function
    v0.0.2

--- include/D3D12Helper.h
+++ include/D3D12Helper.h
@@ -8,4 +8,18 @@ namespace EasyDX12 {
 		queueDesc.Type = D3D12_COMMAND_LIST_TYPE_DIRECT;
 		return device->CreateCommandQueue(&queueDesc, IID_PPV_ARGS(ppCommandQueue));
 	}
+
+	inline HRESULT __cdecl CreateDefaultCopyCommandQueue(_In_ ID3D12Device* device, _COM_Outptr_ ID3D12CommandQueue** ppCommandQueue) {
+		D3D12_COMMAND_QUEUE_DESC queueDesc = {};
+		queueDesc.Flags = D3D12_COMMAND_QUEUE_FLAG_NONE;
+		queueDesc.Type = D3D12_COMMAND_LIST_TYPE_COPY;
+		return device->CreateCommandQueue(&queueDesc, IID_PPV_ARGS(ppCommandQueue));
+	}
+
+	inline HRESULT __cdecl CreateDefaultComputeCommandQueue(_In_ ID3D12Device* device, _COM_Outptr_ ID3D12CommandQueue** ppCommandQueue) {
+		D3D12_COMMAND_QUEUE_DESC queueDesc = {};
+		queueDesc.Flags = D3D12_COMMAND_QUEUE_FLAG_NONE;
+		queueDesc.Type = D3D12_COMMAND_LIST_TYPE_COMPUTE;
+		return device->CreateCommandQueue(&queueDesc, IID_PPV_ARGS(ppCommandQueue));
+	}
 }
