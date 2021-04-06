f35fa8cfdf5bd6f13edb3515700b2ce18bc71984
wangyuzhi233 <78699242+wangyuzhi233@users.noreply.github.com>
2021-04-06 19:24:44 +0800

    Update DXGIHelper.h

--- include/DXGIHelper.h
+++ include/DXGIHelper.h
@@ -3,5 +3,11 @@
 #include <dxgi1_6.h>
 
 namespace EasyDX12 {
-
+	inline HRESULT __cdecl GetHighPerformanceAdapter(_In_ IDXGIFactory* factory, _COM_Outptr_ IDXGIAdapter** ppvAdapter) {
+		IDXGIFactory6* factory6;
+		factory->QueryInterface(&factory6);
+		auto HR = factory6->EnumAdapterByGpuPreference(0, DXGI_GPU_PREFERENCE_HIGH_PERFORMANCE, IID_PPV_ARGS(ppvAdapter));
+		factory6->Release();
+		return HR;
+	}
 }
