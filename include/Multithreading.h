3d2b94b9c64149fecdda7729caf9df2b1922afdc
wangyuzhi233 <78699242+wangyuzhi233@users.noreply.github.com>
2021-04-08 05:13:36 +0800

    add MultithreadingCommandLists interface

--- include/Multithreading.h
+++ include/Multithreading.h
@@ -5,5 +5,16 @@
 #include <dxgi1_6.h>
 #include <thread>
 namespace EasyDX12 {
+	class MultithreadingCommandLists {
+	public:
+		MultithreadingCommandLists(_In_ ID3D12CommandQueue* queue) {}
+		virtual ~MultithreadingCommandLists() {}
 
+		MultithreadingCommandLists(MultithreadingCommandLists&&) noexcept = default;
+		MultithreadingCommandLists& operator=(MultithreadingCommandLists&&) noexcept = default;
+
+		MultithreadingCommandLists(const MultithreadingCommandLists&) = delete;
+		MultithreadingCommandLists& operator=(const MultithreadingCommandLists&) = delete;
+	private:
+	};
 }
