5e3693404a8a52a38f8ebce1be3f62862dd75370
wangyuzhi233 <78699242+wangyuzhi233@users.noreply.github.com>
2021-04-10 01:11:02 +0800

    add framegraph

---
+++ include/FrameGraph.h
@@ -0,0 +1,2 @@
+#pragma once
+namespace EasyDX12 {}
--- include/Multithreading.h
+++ include/Multithreading.h
@@ -1,21 +1,3 @@
 #pragma once
-#pragma comment(lib,"d3d12.lib")
-#include <d3d12.h>
-#include <wrl/client.h>
 namespace EasyDX12 {
-	class MultithreadingCommandLists {
-	public:
-		MultithreadingCommandLists(_In_ ID3D12Device* device) noexcept : mDevice(device) {}
-		virtual ~MultithreadingCommandLists() {}
-
-		MultithreadingCommandLists(MultithreadingCommandLists&&) noexcept = default;
-		MultithreadingCommandLists& operator=(MultithreadingCommandLists&&) noexcept = default;
-
-		MultithreadingCommandLists(const MultithreadingCommandLists&) = delete;
-		MultithreadingCommandLists& operator=(const MultithreadingCommandLists&) = delete;
-
-		void Execute(_In_ ID3D12CommandQueue *queue) {}
-	private:
-		Microsoft::WRL::ComPtr<ID3D12Device> mDevice;
-	};
 }
