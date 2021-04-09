5a55b46a4c40aeb39e319d9eb7ba64fc42ac903c
wangyuzhi233 <78699242+wangyuzhi233@users.noreply.github.com>
2021-04-10 07:40:52 +0800

    Update Multithreading.h
    MultithreadingCommand

--- include/Multithreading.h
+++ include/Multithreading.h
@@ -1,3 +1,17 @@
 #pragma once
+#pragma comment(lib,"d3d12.lib")
+#include <d3d12.h>
+#include <wrl/client.h>
 namespace EasyDX12 {
+	class MultithreadingCommand{
+	public:
+		MultithreadingCommand(_In_ ID3D12Device* device) noexcept : mDevice(device) {}
+		virtual ~MultithreadingCommand() {}
+		MultithreadingCommand(MultithreadingCommand&&) noexcept = default;
+		MultithreadingCommand& operator=(MultithreadingCommand&&) noexcept = default;
+		MultithreadingCommand(const MultithreadingCommand&) = delete;
+		MultithreadingCommand& operator=(const MultithreadingCommand&) = delete;
+	private:
+		Microsoft::WRL::ComPtr<ID3D12Device> mDevice;
+	};
 }
