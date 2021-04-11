e72fa0e4e6ed1d879847c440bee2847cd17570c2
wangyuzhi233 <78699242+wangyuzhi233@users.noreply.github.com>
2021-04-12 06:06:32 +0800

    update

--- include/BaseTypeHelpers.h
+++ include/BaseTypeHelpers.h
@@ -1,8 +1,29 @@
 #pragma once
+#pragma comment(lib,"d3d12.lib")
 #pragma comment(lib,"dxgi.lib")
+#include <d3d12.h>
 #include <dxgi1_6.h>
-
 namespace EasyDX12 {
+	inline HRESULT __cdecl CreateDirectCommandQueue(_In_ ID3D12Device* device, _COM_Outptr_ ID3D12CommandQueue** ppCommandQueue) {
+		D3D12_COMMAND_QUEUE_DESC queueDesc = {};
+		queueDesc.Flags = D3D12_COMMAND_QUEUE_FLAG_NONE;
+		queueDesc.Type = D3D12_COMMAND_LIST_TYPE_DIRECT;
+		return device->CreateCommandQueue(&queueDesc, IID_PPV_ARGS(ppCommandQueue));
+	}
+
+	inline HRESULT __cdecl CreateCopyCommandQueue(_In_ ID3D12Device* device, _COM_Outptr_ ID3D12CommandQueue** ppCommandQueue) {
+		D3D12_COMMAND_QUEUE_DESC queueDesc = {};
+		queueDesc.Flags = D3D12_COMMAND_QUEUE_FLAG_NONE;
+		queueDesc.Type = D3D12_COMMAND_LIST_TYPE_COPY;
+		return device->CreateCommandQueue(&queueDesc, IID_PPV_ARGS(ppCommandQueue));
+	}
+
+	inline HRESULT __cdecl CreateComputeCommandQueue(_In_ ID3D12Device* device, _COM_Outptr_ ID3D12CommandQueue** ppCommandQueue) {
+		D3D12_COMMAND_QUEUE_DESC queueDesc = {};
+		queueDesc.Flags = D3D12_COMMAND_QUEUE_FLAG_NONE;
+		queueDesc.Type = D3D12_COMMAND_LIST_TYPE_COMPUTE;
+		return device->CreateCommandQueue(&queueDesc, IID_PPV_ARGS(ppCommandQueue));
+	}
 	inline HRESULT __cdecl CreateFactory(_COM_Outptr_ IDXGIFactory** ppFactory) {
 		return CreateDXGIFactory2(0, IID_PPV_ARGS(ppFactory));
 	}
--- include/Multithreading.h
+++ include/Multithreading.h
@@ -1,17 +1,4 @@
 #pragma once
-#pragma comment(lib,"d3d12.lib")
-#include <d3d12.h>
-#include <wrl/client.h>
 namespace EasyDX12 {
-	class MultithreadedCommand{
-	public:
-		MultithreadedCommand(_In_ ID3D12Device* device) noexcept : mDevice(device) {}
-		virtual ~MultithreadedCommand() {}
-		MultithreadedCommand(MultithreadedCommand&&) noexcept = default;
-		MultithreadedCommand& operator=(MultithreadedCommand&&) noexcept = default;
-		MultithreadedCommand(const MultithreadedCommand&) = delete;
-		MultithreadedCommand& operator=(const MultithreadedCommand&) = delete;
-	private:
-		Microsoft::WRL::ComPtr<ID3D12Device> mDevice;
-	};
+
 }
