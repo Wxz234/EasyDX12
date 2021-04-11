74f2d90ff55820fbfa49e6d4cba6110c34cd63d8
wangyuzhi233 <78699242+wangyuzhi233@users.noreply.github.com>
2021-04-11 11:48:04 +0800

    Update Multithreading.h
    rename

--- include/Multithreading.h
+++ include/Multithreading.h
@@ -3,14 +3,14 @@
 #include <d3d12.h>
 #include <wrl/client.h>
 namespace EasyDX12 {
-	class MultithreadingCommand{
+	class MultithreadedCommand{
 	public:
-		MultithreadingCommand(_In_ ID3D12Device* device) noexcept : mDevice(device) {}
-		virtual ~MultithreadingCommand() {}
-		MultithreadingCommand(MultithreadingCommand&&) noexcept = default;
-		MultithreadingCommand& operator=(MultithreadingCommand&&) noexcept = default;
-		MultithreadingCommand(const MultithreadingCommand&) = delete;
-		MultithreadingCommand& operator=(const MultithreadingCommand&) = delete;
+		MultithreadedCommand(_In_ ID3D12Device* device) noexcept : mDevice(device) {}
+		virtual ~MultithreadedCommand() {}
+		MultithreadedCommand(MultithreadedCommand&&) noexcept = default;
+		MultithreadedCommand& operator=(MultithreadedCommand&&) noexcept = default;
+		MultithreadedCommand(const MultithreadedCommand&) = delete;
+		MultithreadedCommand& operator=(const MultithreadedCommand&) = delete;
 	private:
 		Microsoft::WRL::ComPtr<ID3D12Device> mDevice;
 	};
