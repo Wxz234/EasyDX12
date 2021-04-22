c27518dc298bd7f1d2b6023b4ac492eb93574617
wangyuzhi233 <78699242+wangyuzhi233@users.noreply.github.com>
2021-04-22 22:18:58 +0800

    add fxaa

--- include/FXAA.h
+++ include/FXAA.h
@@ -1,2 +1,14 @@
 #pragma once
-namespace EasyDX12 {}
+namespace EasyDX12 {
+	class FXAA {
+	public:
+		FXAA() {}
+		~FXAA() {}
+		FXAA(FXAA&&) noexcept = default;
+		FXAA& operator=(FXAA&&) noexcept = default;
+		FXAA(const FXAA&) = delete;
+		FXAA& operator=(const FXAA&) = delete;
+	private:
+	};
+
+}
