df4b20e5d98bdb2c7348873740d6ed7fbc2d01b3
wangyuzhi233 <429373960@qq.com>
2021-04-27 16:17:28 +0800

    FIX BUG

--- include/EasyDX12/AdapterHelpers.h
+++ include/EasyDX12/AdapterHelpers.h
@@ -1,45 +1,99 @@
 #pragma once
 #pragma comment(lib,"dxgi.lib")
 #include <dxgi1_6.h>
+#include <wrl/client.h>
 #include <string>
 namespace EasyDX12 {
 	inline HRESULT __cdecl GetWarpAdapter(_In_ IDXGIFactory* factory, _COM_Outptr_ IDXGIAdapter** ppvAdapter) {
-		IDXGIFactory4* factory4;
-		factory->QueryInterface(&factory4);
-		auto HR = factory4->EnumWarpAdapter(IID_PPV_ARGS(ppvAdapter));
-		factory4->Release();
-		return HR;
+		if (!ppvAdapter)
+			return E_INVALIDARG;
+		*ppvAdapter = nullptr;
+		if (!factory)
+			return E_INVALIDARG;
+
+		Microsoft::WRL::ComPtr<IDXGIFactory> myfactory(factory);
+		Microsoft::WRL::ComPtr<IDXGIFactory4> myfactory4;
+		HRESULT hr = myfactory.As(&myfactory4);
+		if (FAILED(hr))
+			return hr;
+		Microsoft::WRL::ComPtr<IDXGIAdapter> myadapter;
+		hr = myfactory4->EnumWarpAdapter(IID_PPV_ARGS(&myadapter));
+		if (FAILED(hr))
+			return hr;
+		*ppvAdapter = myadapter.Detach();
+		return S_OK;
 	}
+
 	inline HRESULT __cdecl GetDefaultAdapter(_In_ IDXGIFactory* factory, _COM_Outptr_ IDXGIAdapter** ppvAdapter) {
-		return factory->EnumAdapters(0, ppvAdapter);
+		if (!ppvAdapter)
+			return E_INVALIDARG;
+		*ppvAdapter = nullptr;
+		if (!factory)
+			return E_INVALIDARG;
+		Microsoft::WRL::ComPtr<IDXGIAdapter> myadapter;
+		HRESULT hr = factory->EnumAdapters(0, &myadapter);
+		if (FAILED(hr))
+			return hr;
+		*ppvAdapter = myadapter.Detach();
+		return S_OK;
 	}
+
 	inline HRESULT __cdecl GetHighPerformanceAdapter(_In_ IDXGIFactory* factory, _COM_Outptr_ IDXGIAdapter** ppvAdapter) {
-		IDXGIFactory6* factory6;
-		factory->QueryInterface(&factory6);
-		auto HR = factory6->EnumAdapterByGpuPreference(0, DXGI_GPU_PREFERENCE_HIGH_PERFORMANCE, IID_PPV_ARGS(ppvAdapter));
-		factory6->Release();
-		return HR;
+		if (!ppvAdapter)
+			return E_INVALIDARG;
+		*ppvAdapter = nullptr;
+		if (!factory)
+			return E_INVALIDARG;
+
+		Microsoft::WRL::ComPtr<IDXGIFactory> myfactory(factory);
+		Microsoft::WRL::ComPtr<IDXGIFactory6> myfactory6;
+		HRESULT hr = myfactory.As(&myfactory6);
+		if (FAILED(hr))
+			return hr;
+		Microsoft::WRL::ComPtr<IDXGIAdapter> myadapter;
+		hr = myfactory6->EnumAdapterByGpuPreference(0, DXGI_GPU_PREFERENCE_HIGH_PERFORMANCE, IID_PPV_ARGS(&myadapter));
+		if (FAILED(hr))
+			return hr;
+		*ppvAdapter = myadapter.Detach();
+		return S_OK;
 	}
 	inline HRESULT __cdecl GetMinimumPowerAdapter(_In_ IDXGIFactory* factory, _COM_Outptr_ IDXGIAdapter** ppvAdapter) {
-		IDXGIFactory6* factory6;
-		factory->QueryInterface(&factory6);
-		auto HR = factory6->EnumAdapterByGpuPreference(0, DXGI_GPU_PREFERENCE_MINIMUM_POWER, IID_PPV_ARGS(ppvAdapter));
-		factory6->Release();
-		return HR;
+		if (!ppvAdapter)
+			return E_INVALIDARG;
+		*ppvAdapter = nullptr;
+		if (!factory)
+			return E_INVALIDARG;
+
+		Microsoft::WRL::ComPtr<IDXGIFactory> myfactory(factory);
+		Microsoft::WRL::ComPtr<IDXGIFactory6> myfactory6;
+		HRESULT hr = myfactory.As(&myfactory6);
+		if (FAILED(hr))
+			return hr;
+		Microsoft::WRL::ComPtr<IDXGIAdapter> myadapter;
+		hr = myfactory6->EnumAdapterByGpuPreference(0, DXGI_GPU_PREFERENCE_MINIMUM_POWER, IID_PPV_ARGS(&myadapter));
+		if (FAILED(hr))
+			return hr;
+		*ppvAdapter = myadapter.Detach();
+		return S_OK;
 	}
 
 	inline std::wstring __cdecl GetAdapterName(_In_ IDXGIAdapter* adapter) {
-		if (adapter) {
-			DXGI_ADAPTER_DESC desc;
-			adapter->GetDesc(&desc);
-			return std::wstring(desc.Description);
-		}
-		return L"";
+		if (!adapter)
+			return L"";
+		DXGI_ADAPTER_DESC desc;
+		HRESULT hr = adapter->GetDesc(&desc);
+		if (FAILED(hr))
+			return L"";
+		return std::wstring(desc.Description);
 	}
 
 	inline bool __cdecl IsNVIDIAAdapter(_In_ IDXGIAdapter* adapter) {
+		if (!adapter)
+			return false;
 		DXGI_ADAPTER_DESC desc;
-		adapter->GetDesc(&desc);
+		HRESULT hr = adapter->GetDesc(&desc);
+		if (FAILED(hr))
+			return false;
 		if (desc.VendorId == 0x10DE) {
 			return true;
 		}
@@ -47,8 +101,12 @@ namespace EasyDX12 {
 	}
 
 	inline bool __cdecl IsAMDAdapter(_In_ IDXGIAdapter* adapter) {
+		if (!adapter)
+			return false;
 		DXGI_ADAPTER_DESC desc;
-		adapter->GetDesc(&desc);
+		HRESULT hr = adapter->GetDesc(&desc);
+		if (FAILED(hr))
+			return false;
 		if (desc.VendorId == 0x1002) {
 			return true;
 		}
@@ -56,8 +114,12 @@ namespace EasyDX12 {
 	}
 
 	inline bool __cdecl IsIntelAdapter(_In_ IDXGIAdapter* adapter) {
+		if (!adapter)
+			return false;
 		DXGI_ADAPTER_DESC desc;
-		adapter->GetDesc(&desc);
+		HRESULT hr = adapter->GetDesc(&desc);
+		if (FAILED(hr))
+			return false;
 		if (desc.VendorId == 0x8086) {
 			return true;
 		}
