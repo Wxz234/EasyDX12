e82a0295c15e302fe773e3f013430b566c1a9a8e
wangyuzhi233 <78699242+wangyuzhi233@users.noreply.github.com>
2021-06-22 11:30:01 +0800

    Update EasyDX12.h

--- include/EasyDX12.h
+++ include/EasyDX12.h
@@ -3,6 +3,7 @@
 #pragma comment(lib,"dxgi.lib")
 #pragma comment(lib,"dxguid.lib")
 #include <d3d12.h>
+#include <d3d12sdklayers.h>
 #include <dxgi1_6.h>
 #include <wrl/client.h>
 #include <wrl/wrappers/corewrappers.h>
